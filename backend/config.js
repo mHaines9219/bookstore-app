ffba458c8babbf9c7151fb2536df1c53b47da0bb
mHaines331 <mhaines9219@gmail.com>
2024-02-06 19:31:09 -0800

    adjust model

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5002;
+
+export const mongoDBURL = `mongodb+srv://seistudent:password1234@bookstore-mern.tzwi8fs.mongodb.net/?retryWrites=true&w=majority`;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,40 @@
 import express from 'express';
-import { PORT } from './config.js';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log(req);
+  return res.status(234).send('Hello World');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//Route for Save new book
+app.post('/book', (req, res) => {
+  try {
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      rel_yr: req.body.rel_yr,
+    };
+    const book = new Book(newBook);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
